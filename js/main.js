e79906885e22994034e0c3e27eca88c31aa60cb6
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-04-14 15:31:49 +0200

    add snack 2

--- js/main.js
+++ js/main.js
@@ -2,23 +2,24 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
-const biciLeggeraEL = document.getElementById("bici-leggera");
-const bici = [
-  {
-    nome: "mountain bike",
-    peso: 60,
-  },
-  { nome: "trial bike", peso: 50 },
 
-  { nome: "running bike", peso: 40 },
-];
-let biciLeggera = bici[0];
-for (let i = 0; i < bici.length; i++) {
-  if (bici[i].peso < biciLeggera.peso) {
-    biciLeggera = bici[i];
-    biciLeggeraEL.innerText = `La bici più leggera è: ${biciLeggera.nome} grazie al suo peso di soli ${biciLeggera.peso} Kg`;
-  }
-}
+// const biciLeggeraEL = document.getElementById("bici-leggera");
+// const bici = [
+//   {
+//     nome: "mountain bike",
+//     peso: 60,
+//   },
+//   { nome: "trial bike", peso: 50 },
+
+//   { nome: "running bike", peso: 40 },
+// ];
+// let biciLeggera = bici[0];
+// for (let i = 0; i < bici.length; i++) {
+//   if (bici[i].peso < biciLeggera.peso) {
+//     biciLeggera = bici[i];
+//     biciLeggeraEL.innerText = `La bici più leggera è: ${biciLeggera.nome} grazie al suo peso di soli ${biciLeggera.peso} Kg`;
+//   }
+// }
 
 // Snack2
 // Creare un array di oggetti di squadre di calcio.
@@ -26,3 +27,34 @@ for (let i = 0; i < bici.length; i++) {
 // Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const squadre = [
+  {
+    nome: "Juventus",
+    puntiFatti: generateRandomNumber(1, 100),
+    falliSubiti: generateRandomNumber(1, 30),
+  },
+  {
+    nome: "Inter",
+    puntiFatti: generateRandomNumber(1, 100),
+    falliSubiti: generateRandomNumber(1, 30),
+  },
+  {
+    nome: "Milan",
+    puntiFatti: generateRandomNumber(1, 100),
+    falliSubiti: generateRandomNumber(1, 30),
+  },
+];
+
+console.log(squadre[0].puntiFatti);
+
+let newArray = [];
+
+for (let i = 0; i < squadre.length; i++) {
+  newArray.push(squadre[i].nome);
+  newArray.push(squadre[i].falliSubiti);
+  console.log(newArray);
+}
