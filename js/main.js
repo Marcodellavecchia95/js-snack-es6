ede7597e02ce9e1dabf4a6e8450f95bdc0685aa3
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-04-14 15:13:50 +0200

    add snack 1

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+const biciLeggeraEL = document.getElementById("bici-leggera");
+const bici = [
+  {
+    nome: "mountain bike",
+    peso: 60,
+  },
+  { nome: "trial bike", peso: 50 },
+
+  { nome: "running bike", peso: 40 },
+];
+let biciLeggera = bici[0];
+for (let i = 0; i < bici.length; i++) {
+  if (bici[i].peso < biciLeggera.peso) {
+    biciLeggera = bici[i];
+    biciLeggeraEL.innerText = `La bici più leggera è: ${biciLeggera.nome} grazie al suo peso di soli ${biciLeggera.peso} Kg`;
+  }
+}
 
 // Snack2
 // Creare un array di oggetti di squadre di calcio.
